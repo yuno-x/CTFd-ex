2add2594fac7818324787f881b542171eb6f5ac1
ishituka <ishitsuka.sc@gmail.com>
2024-06-29 08:13:00 +0900

    add explanation column
    Signed-off-by: ishituka <ishitsuka.sc@gmail.com>

--- CTFd/plugins/challenges/__init__.py
+++ CTFd/plugins/challenges/__init__.py
@@ -53,6 +53,7 @@ class BaseChallenge(object):
             "name": challenge.name,
             "value": challenge.value,
             "description": challenge.description,
+            "explanation": challenge.explanation,
             "connection_info": challenge.connection_info,
             "next_id": challenge.next_id,
             "category": challenge.category,
